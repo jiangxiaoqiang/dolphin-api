48256c9c8b556fd1e651ba04460741db99e59c8a
jiangxiaoqiang <jiangtingqiang@gmail.com>
2017-07-02 13:30:19 +0800

    daily

--- api/src/main/java/dolphin/api/controllers/BookController.java
+++ api/src/main/java/dolphin/api/controllers/BookController.java
@@ -1,32 +1,46 @@
 package dolphin.api.controllers;
 
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import service.BookService;
+import model.Book;
 
 /**
  * Created by dolphin on 13/6/2017.
  */
 //表明这是一个 Controller
-@Controller
+//@Controller
 
 //RestController是一种Rest风格的Controller，可以直接返回对象而不返回视图，返回的对象可以使JSON，XML等
-//@RestController
-
-//使用自动配置，主动添加并解析bean，配置文件等信息
-@EnableAutoConfiguration
+@RestController
 public class BookController {
-    //设置访问的url
-    @RequestMapping("/")
-    //表示返回JSON格式的结果，如果前面使用的是@RestController可以不用写
-    @ResponseBody
-    String home() {
-        return "Hello World 发发分为无法lwjfwlfjwe";//返回结果为字符串
+
+    @Autowired
+    @Qualifier(value = "bookService")
+    private BookService bookService;
+
+    /**
+     * @param isbn
+     * @return
+     */
+    @GetMapping("isbn")
+    public Book getBookByISBN(@RequestParam String isbn) {
+        Book book = bookService.getBookByISBN(isbn);
+        return book;
     }
 
-    public static void main(String[] args) throws Exception {
-        //通过SpringApplication的run()方法启动应用，无需额外的配置其他的文件
-        SpringApplication.run(BookController.class, args);
+    /**
+     * @param id
+     * @return
+     */
+    @GetMapping("id")
+    public Book getBookByISBN(@RequestParam Long id) {
+        Book book = bookService.getBookById(id);
+        return book;
     }
 }
