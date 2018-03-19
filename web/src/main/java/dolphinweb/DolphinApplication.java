fe52408bdbbf88d129136812be534a0dd1c75ba2
Jiang Xiaoqiang <xiaoqiang.jiang@hualongdata.com>
2018-03-19 21:55:04 +0800

    tweak

--- web/src/main/java/dolphinweb/DolphinApplication.java
+++ web/src/main/java/dolphinweb/DolphinApplication.java
@@ -1,5 +1,6 @@
 package dolphinweb;
 
+import model.Book;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by dolphin on 23/6/2017.
@@ -23,8 +27,17 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class DolphinApplication {
     public static void main(String[] args) throws Exception {
-        SpringApplication app = new SpringApplication(DolphinApplication.class);
-        app.addListeners(new ApplicationPidFileWriter("app.pid"));
-        app.run(args);
+        Book book1 = new Book();
+        book1.setId((long) 1);
+        List<Book> bookList = new ArrayList();
+        bookList.add(book1);
+        long sumId = bookList.stream()
+                .mapToLong(Book::getId)
+                .sum();
+
+
+        //SpringApplication app = new SpringApplication(DolphinApplication.class);
+        //app.addListeners(new ApplicationPidFileWriter("app.pid"));
+        //app.run(args);
     }
 }
