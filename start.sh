c6b11730dcee68149fce52a32a486112d88db7ca
Jiang Xiaoqiang <xiaoqiang.jiang@hualongdata.com>
2018-03-26 23:00:17 +0800

    daily

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ count=`ps -ef | grep dolphin-web-${VERSION} | grep -v "grep" | wc -l`
 if [ $count -lt 1 ]; then
 	nohup ${JAVA_HOME}/bin/java -Xmx512M -Xms256M \
 	-jar -Xdebug -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=5005	\
-	${APP_PATH}/dolphin-web-{VERSION}.jar \
+	${APP_PATH}/dolphin-web-${VERSION}.jar \
 	--spring.config.location=application.properties>/dev/null &
 else
 	echo "process aready exists!"
