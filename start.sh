5cf170cd610dee17edfecbf6d428db8bb7edf506
Jiang Xiaoqiang <xiaoqiang.jiang@hualongdata.com>
2018-03-26 23:11:29 +0800

    daily

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 #nohup java -Xmx8192M -Xms4096M -jar credit-system-web-boot-1.0.0.jar --spring.config.location=application-jjxxzx-test.properties >/dev/null &
 
 readonly JAVA_HOME='/opt/dolphin/jdk1.8.0_161'
-readonly APP_PATH='/opt/dolphin/backend'
+readonly APP_PATH='/opt/dolphin/app/backend'
 
 source ${APP_PATH}/version.properties
 
@@ -12,7 +12,7 @@ if [ $count -lt 1 ]; then
 	nohup ${JAVA_HOME}/bin/java -Xmx512M -Xms256M \
 	-jar -Xdebug -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=5005	\
 	${APP_PATH}/dolphin-web-${VERSION}.jar \
-	--spring.config.location=application.properties>/dev/null &
+	--spring.config.location=${APP_PATH}/application.properties>/dev/null &
 else
 	echo "process aready exists!"
 fi
